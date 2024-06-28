26a50f4b66521c62d45ee9b058a0ec6d97679d7c
mohamed hosam <mohamedhosam@MacBook-Pro-mohamed.local>
2024-06-28 03:37:06 +0300

    hi

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
